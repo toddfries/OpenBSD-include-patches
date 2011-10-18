5df5f010b1d2ee48a3faca01b1184c1eba567bae
Todd T. Fries <todd@fries.net>
2011-10-18 14:21:57 -0500

    sync

--- wchar.h
+++ wchar.h
@@ -1,4 +1,4 @@
-/*	$OpenBSD: wchar.h,v 1.19 2011/07/04 04:37:34 nicm Exp $	*/
+/*	$OpenBSD: wchar.h,v 1.21 2011/10/16 13:22:06 stsp Exp $	*/
 /*	$NetBSD: wchar.h,v 1.16 2003/03/07 07:11:35 tshiozak Exp $	*/
 
 /*-
@@ -156,6 +156,8 @@ int wcsncasecmp(const wchar_t *, const wchar_t *, size_t);
 #if __ISO_C_VISIBLE >= 1999
 float	wcstof(const wchar_t * __restrict, wchar_t ** __restrict);
 long double wcstold(const wchar_t * __restrict, wchar_t ** __restrict);
+size_t wcsftime(wchar_t * __restrict, size_t, const wchar_t *__restrict,
+	const struct tm *__restrict);
 #endif
 
 #if (defined(__GNUC__) && __GNUC__ >= 2 && !defined(__STRICT_ANSI__)) || \
@@ -187,6 +189,13 @@ int vswprintf(wchar_t * __restrict, size_t, const wchar_t * __restrict,
 	__va_list);
 int vwprintf(const wchar_t * __restrict, __va_list);
 int wprintf(const wchar_t * __restrict, ...);
+
+int fwscanf(FILE * __restrict, const wchar_t * __restrict, ...);
+int swscanf(const wchar_t * __restrict, const wchar_t * __restrict, ...);
+int vfwscanf(FILE * __restrict, const wchar_t * __restrict, __va_list);
+int vswscanf(const wchar_t * __restrict, const wchar_t * __restrict, __va_list);
+int vwscanf(const wchar_t * __restrict, __va_list);
+int wscanf(const wchar_t * __restrict, ...);
 
 #define getwc(f) fgetwc(f)
 #define getwchar() getwc(stdin)
